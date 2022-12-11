66212f2cb7b0ad6b1af594f791b2e53df234da61
Heba_Doleh <hdoleh14@gmail.com>
2022-12-11 12:59:35 -0600

    adding a new variable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// variables
+
 const colorBtn1 = document.querySelector('.colorBtn');
 const bodypcg =document.querySelector("body");
 const color =['yellow','red','green','blue','brown','gray'];
@@ -7,7 +9,7 @@ const color =['yellow','red','green','blue','brown','gray'];
 colorBtn1.addEventListener('click',changeColor);
 
 function changeColor(){
-//bodypcg.style.backgroundColor=color[1];
+bodypcg.style.backgroundColor=color[1];
 let random = Math.floor(Math.random()* color.length);
 bodypcg.style.backgroundColor=color[random];
 
