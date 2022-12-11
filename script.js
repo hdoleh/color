79d4c1e75f790d0eb959bfa0f895427243e31f15
Heba_Doleh <hdoleh14@gmail.com>
2022-12-11 08:22:05 -0600

    Added project.css

---
+++ script.js
@@ -0,0 +1,20 @@
+const colorBtn1 = document.querySelector('.colorBtn');
+const bodypcg =document.querySelector("body");
+const color =['yellow','red','green','blue','brown','gray'];
+
+
+
+colorBtn1.addEventListener('click',changeColor);
+
+function changeColor(){
+//bodypcg.style.backgroundColor=color[1];
+let random = Math.floor(Math.random()* color.length);
+bodypcg.style.backgroundColor=color[random];
+
+
+
+
+
+
+
+}
