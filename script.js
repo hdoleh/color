bae1904e5e111ffeabe39df54a57f45ce6ab8613
Heba_Doleh <hdoleh14@gmail.com>
2022-12-22 19:57:38 -0600

    changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-variables
+//variables
 
 const colorBtn1 = document.querySelector('.colorBtn');
 const bodypcg =document.querySelector("body");
- creating an array to pick a color
+ //creating an array to pick a color
 const color =['yellow','red','green','blue','brown','gray'];
 
 //changes
